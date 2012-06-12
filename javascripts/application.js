b40810dd1ae01cadcd99f43a0c85f97caf385f8f
Brian Galok <brian.galok@gmail.com>
2012-06-11 23:18:03 -0400

    added timers buttons, countdown is not working

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,5 +1,97 @@
-// time 54.41
- $.fn.extend({
+//
+
+var CountdownTimer = {
+	endTime:null,
+	timerId:null,
+	minutes:0,
+	maxWidth:0,
+	maxTime:(25 * 60 * 1000),
+	isBreak:false,
+	
+	normalColors:["#e47a80", "#eb363b"],
+	breakColors:["#97c751", "#759f58"],
+	
+	start: function(minutes, isBreak) { 
+		if(this.maxWidth ==0)
+			this.maxWidth =$('#timer-bar').width();
+			
+		if(this.timerId)
+			this.cancel();
+			
+		this.minutes = minutes;
+		this.isBreak = isBreak;
+		
+		this.setupTimer();
+		this.flashBar();
+	},
+
+	setupTimer:function() {
+		$('#timer-finished').fadeOut();
+	
+		var date = new Date();
+		this.endTime = date.getTime() + (this.minutes * 60 * 1000);
+		this.timerId = setInterval(this.tick, 1000);
+	
+		this.setBarToTime(this.minutes * 60 * 1000);	
+		$('title').text("Started " + 
+			(this.isBreak ? this.minutes + " minute break." : "pomodoro."));
+	},
+
+	flashBar:function() {
+		var lightColor, darkColor;
+		if(this.isBreak)
+		{
+			lightColor = this.breakColors[0];
+			darkColor = this.breakColors[1];
+		} else {
+			lightColor = this.breakColors[0];
+			darkColor = this.breakColors[1];
+		}
+		$('#timer-bar').animate({backgroundColor:lightColor})
+			.animate({backgroundColor:darkColor});		
+	},
+
+	setBarToTime:function(time) {
+		$('#timer-bar').animate({width:(time/this.maxTime) * this.maxWidth});
+	},
+
+	cancel:function() {
+		clearInterval(this.timerId);
+	},
+
+	tick:function() {
+		CountdownTimer.tock();
+	},
+
+	tock:function() {
+		var date = new Date();
+		var remainingTime = this.endtime - date.getTime();
+		if(remainingTime <= 0) {
+			this.cancel();
+			this.setBarToTime(0);
+			this.showCompletedInLog();
+			this.showCompletedMessage();
+		} else {
+			this.setBarToTime(remainingTime);
+		}
+	},
+
+	showCompletedInLog:function() {
+		var logClass = "timer-log-" + this.minutes + (this.isBreak ? "-break" : "");
+	
+		$('#timer').append('div id="timer-finished"></div>');
+	},
+
+	showCompletedMessage:function() {
+		if($('#timer-finished').length == 0)
+			$('timer').append('<div id="timer-finished"></div>');
+		var message = "Finished " + (this.isBreak ? this.minutes + " minute break." : "one pomodoro.");
+		$('title').text(message);
+		$('#timer-finished').text(message).fadeIn(1000);
+	},
+};
+	
+$.fn.extend({
 
 	taskStates:['task-empty', 'task-x', 'task-apostrophe', 'task-dash'],
 	
@@ -11,7 +103,12 @@
 		return this;
 	},
 	
-	resetTaskState:function() { },
+	resetTaskState:function() { 
+		this.resetTaskStateClassNames();
+		return this.each(function() {
+			$(this).data('taskStateIndex',0).addClass($.fn.taskStates[0]);
+		});
+	},
 	
 	toggleTaskState:function() { 
 		this.resetTaskStateClassNames();
@@ -30,6 +127,20 @@
 
 $(function () {
 
+	$('#button-25').click(function(e) {
+		e.preventDefault();
+		CountdownTimer.start(25);
+	});
+
+	$('#button-5-break').click(function(e) {
+		e.preventDefault();
+		CountdownTimer.start(5, true);
+	});
+
+	$('#button-25-break').click(function(e) {
+		e.preventDefault();
+		CountdownTimer.start(25, true);
+	});
 	$('.completion a').live("click", function(e){
 		$(this).toggleTaskState();
 		return false;
@@ -37,7 +148,10 @@ $(function () {
 	
 	$('#add').click(function(e) { 
 		var taskItem = $('#tasks ul li:first').clone();
-		taskItem.find('input[type="text"]').val("");
+		taskItem
+				.find('.completion a').resetTaskState()
+			.end()
+				.find('input[type="text"]').val("");
 		$('#tasks ul').append(taskItem);
 		taskItem.find('input[type="text"]:first').focus();
 		return false;
